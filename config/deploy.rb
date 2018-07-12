d5874dff1c803e64fc91ff4e271d6b26833a2c9d
Scott Johnson <fuzzygroup@gmail.com>
2018-07-12 07:48:07 -0400

    Update to deploy for proper restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -113,6 +113,25 @@ set :keep_releases, 2
 # set :ssh_options, verify_host_key: :secure
 
 namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+  
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
