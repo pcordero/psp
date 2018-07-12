395adc24f000275d1167a510e4833d466c442165
Scott Johnson <fuzzygroup@gmail.com>
2018-07-12 18:02:45 -0400

    fixes to photos

--- app/models/leader.rb
+++ app/models/leader.rb
@@ -41,9 +41,17 @@ class Leader < Hashie::Mash
     return "http://placehold.it/109x148" if self['photo_file'].nil?
     return "http://placehold.it/109x148" if self['photo_path'].nil? 
     
+    # works: http://18.191.175.149/photos/SL/AK/H/Birch_Chris_583178.jpg
+    
+    @leader.photo_path
+    => "Images\\Photos\\FL\\H"
+    
+    photo_path = self.photo_src.sub(/Images/,'').sub(/Photos/,'photos').sub(/\\\\/,'/')
+    
     #return self['photo_src'] || "http://placehold.it/109x148"
     #return self['photo_src'] || "placeholder.png"
-    ps = "#{self['photo_path'].sub(/Images\/\//,'public/')}#{self['photo_file']}"
+    #ps = "#{self['photo_path'].sub(/Images\/\//,'public/')}#{self['photo_file']}"
+    ps = "#{photo_path}/#{photo_file}"
     #self['photo_src'] || "placeholder.jpg"
     if self['photo_file']
       return ps
