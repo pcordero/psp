f440a5f8acb9df804c3ebb068f55868b13c48a55
Scott Johnson <fuzzygroup@gmail.com>
2018-07-17 20:55:09 -0400

    Image fixes

--- app/models/leader.rb
+++ app/models/leader.rb
@@ -56,7 +56,7 @@ class Leader < Hashie::Mash
     p_path = self.photo_path.sub(/Images/,'').sub(/Photos/,'photos').gsub(/\\/,'/')
     l.info("p_path = #{p_path}")
     parts = self["photo_file"].split("_")
-    tmp1 = File.join(Rails.root, p_path, "*_#{parts.last}")
+    tmp1 = File.join(Rails.root, "public", p_path, "*_#{parts.last}")
     l.info("tmp1 = #{tmp1}")
     results = Dir.glob(File.join(Rails.root, p_path, "*_#{parts.last}"))
     
