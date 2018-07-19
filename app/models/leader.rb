1ef530505f83b8e75aea207114bfaa4cc24232ab
Scott Johnson <fuzzygroup@gmail.com>
2018-07-19 09:33:05 -0400

    Fix to pathing - replace SLE w/ SL

--- app/models/leader.rb
+++ app/models/leader.rb
@@ -60,9 +60,11 @@ class Leader < Hashie::Mash
     l.info("tmp1 = #{tmp1}")
     results = Dir.glob(File.join(Rails.root, "public", p_path, "*#{parts.last}"))
     
+    l.info("p_path BEFORE = #{p_path}")
     if p_path =~ /SLE/
       p_path = p_path.sub(/SLE/, 'SL')
     end
+    l.info("p_path AFTER = #{p_path}")
     #results = Dir.glob(Rails.root, "public/photos") ("/Users/sjohnson/fuzzygroup/consulting/new_leaders_original/psp/public/photos/SL/IN/S/*194425.jpg")
     if results && results.size == 1
       file_by_number = results.first
