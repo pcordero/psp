cafa9cd73ed3c7c7040fb642b8cf02ba089dc698
Scott Johnson <fuzzygroup@gmail.com>
2018-08-30 19:21:49 -0400

    Rollup of recent changes

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -3,6 +3,10 @@ class StatesController < ApplicationController
   def index
     render layout: "application"
   end
+  
+  def all
+    @states = %w{al ak az ar ca co ct de fl ga hi id il in ia ks ky la me md ma mi mn ms mo mt ne nv nh nj nm ny nc nd oh ok or pa ri sc sd tn tx ut vt va wa wv wi wy}
+  end
 
   def show
     cookies[:state_code] = params[:id]
